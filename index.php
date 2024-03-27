6e095e73245707bf7d04018fc54f84478ce420c3
gregrr@gmail.com <gregrr@gmail.com>
2024-03-27 09:16:31 -0400

    ran php through formatter

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
-<?php  
-    include_once 'functions.php';
-    include_once 'keys.php';
-
-    // available models (3/27/2024): 
-    $models = array('mixtral-8x7b-32768', 'llama2-70b-4096', 'gemma-7b-it');
-
-    $prompt         = "What's the best way to cook a hotdog?";
-
-    echo   "<h2>Original Prompt</h2>
+<?php
+  include_once 'functions.php';
+  include_once 'keys.php';
+  
+  // available models (3/27/2024): 
+  $models = array(
+       'mixtral-8x7b-32768',
+      'llama2-70b-4096',
+      'gemma-7b-it' 
+  );
+  
+  $prompt = "What's the best way to cook a hotdog?";
+  
+  echo "<h2>Original Prompt</h2>
             <pre>$prompt</pre><hr><hr><hr>";
-
-    foreach($models as $model){
-        $groq_result    = groq_call($prompt, $model, 0.5, $groq_key); //returns an array that we converted from a json
-        $groq_reply     = $groq_result["choices"][0]["message"]["content"];
-
-        $cost = groq_cost($groq_result); // cost is in dollars 
-
-        
-        echo    "<h2>$model Response</h2>
+  
+  foreach ( $models as $model ) {
+      $groq_result = groq_call( $prompt, $model, 0.5, $groq_key ); //returns an array that we converted from a json
+      $groq_reply  = $groq_result[ "choices" ][ 0 ][ "message" ][ "content" ];
+      
+      $cost = groq_cost( $groq_result ); // cost is in dollars 
+      
+      echo "<h2>$model Response</h2>
                 <pre>$groq_reply</pre>
-                <p><strong>Cost: ".round($cost *100 ,3)."&cent;</p>" .  // convert to cents and round to 3 decimal places
-                "<hr><br><br>";
-    }
-
+                <p><strong>Cost: " . round( $cost * 100, 3 ) . "&cent;</p>" . // convert to cents and round to 3 decimal places
+          "<hr><br><br>";
+  }
 ?>
